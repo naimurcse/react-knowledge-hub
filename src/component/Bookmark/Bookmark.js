a6912f9592317238667dc58469338b4526e38d9c
naimurcse <2015naim@gmail.com>
2023-11-21 01:31:43 +0600

    Make up is done for Remark component

--- src/component/Bookmark/Bookmark.js
+++ src/component/Bookmark/Bookmark.js
@@ -4,7 +4,28 @@ import "./Bookmark.css";
 const Bookmark = () => {
    return (
       <div>
-         <h2>Bookmarks</h2>
+         <div className="spent-time">
+            <h2 className="spent-time__text">Spent time on read : 177 min</h2>
+         </div>
+
+         <div className="bookmark">
+            <h3>Bookmarked Blogs : 8</h3>
+            <div className="bookmark__title-container">
+               <h4 className="bookmark__title">
+                  Master Microsoft Power Platform and Become an In-Demand!
+               </h4>
+            </div>
+            <div className="bookmark__title-container">
+               <h4 className="bookmark__title">
+                  Master Microsoft Power Platform and Become an In-Demand!
+               </h4>
+            </div>
+            <div className="bookmark__title-container">
+               <h4 className="bookmark__title">
+                  Master Microsoft Power Platform and Become an In-Demand!
+               </h4>
+            </div>
+         </div>
       </div>
    );
 };
